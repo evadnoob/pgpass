572c757926bb8f1e0881fd38b75e63385d7c4555
David Boon <david.boon@gmail.com>
2015-08-26 12:40:46 -0700

    a simple read-pgpass program.

---
+++ src/bin/read-pgpass.rs
@@ -0,0 +1,100 @@
+extern crate regex;
+
+use std::error::Error;
+use std::fs::File;
+use std::path::Path;
+use std::env;
+use std::io::prelude::*;
+use regex::Regex;
+
+#[derive(Debug, Clone)]
+struct PgPassEntry<'a> {
+    username: &'a str,
+    hostname: &'a str,
+    port: &'a str,
+    database: &'a str,
+    password: &'a str
+}
+
+ fn main() {
+    for argument in env::args() {
+        println!("{}", argument);
+    }
+    
+    match env::args().last() {
+        Some(arg) => {
+            println!("arg {}", arg);
+            let pgpass_entry_match = read_pgpass_file("~/.pgpass", "auser", "a.host.name", "5432", "database");
+            println!("pgpass_entry_match {:?}", pgpass_entry_match);
+        },
+        _ => {
+            println!("missing required command line arguments.")
+        }
+    }
+ }
+
+fn read_pgpass_file<'a>(path_to_pgpass: &str, username: &str, hostname: &str, port: &str, database: &str) -> Option<&'a PgPassEntry<'a>> {
+
+    let path = Path::new(path_to_pgpass);
+    let display = path.display();
+
+    // Open the path in read-only mode, returns `io::Result<File>`
+    let mut file = match File::open(&path) {
+        // The `description` method of `io::Error` returns a string that
+        // describes the error
+        Err(why) => panic!("couldn't open {}: {}", display,
+                           Error::description(&why)),
+        Ok(file) => file,
+    };
+
+    let mut pgpass_entries: Vec<PgPassEntry> = Vec::new();
+    
+    let mut reader = std::io::BufReader::new(&file);
+    let re = Regex::new(r":").unwrap();
+
+    for line in reader.lines() {
+        println!("line {:?}", line);
+
+        match line {
+            Ok(ref l) => {
+                let mut parts = re.split(l);
+        
+                let host = parts.next().unwrap();
+                println!("parts[0] {:?}", host);
+
+                let port = parts.next().unwrap();
+                println!("parts[1] {:?}", port);
+                
+                let database = parts.next().unwrap();
+                println!("parts[2] {:?}", database);
+                
+                let username = parts.next().unwrap();
+                println!("parts[3] {:?}", username);
+                
+                let password = parts.next().unwrap();
+                println!("parts[4] {:?}", password);
+
+                
+                let mut parts_vec = re.split(l).collect::<Vec<&str>>();
+                println!("parts_vec {:?}", parts_vec);
+
+                pgpass_entries.push(PgPassEntry{
+                    hostname: &host,
+                    port: &port,
+                    username: &username,
+                    database: &database,
+                    password:  &password});
+
+                
+            },
+            Err(e) => println!("no matching line {}", e)
+        }
+    }
+
+    //println!("pgpass_entries {:?}", pgpass_entries);
+
+    pgpass_entries.iter().find(|&x| x.hostname == hostname
+                                                        && x.database == database
+                                                        && x.port == port
+                                                        && x.username == username)
+}
